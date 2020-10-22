9979632dc61e3d22a41f8e01fcc96791039fd94d
Weiwei <tyler22501789@gmail.com>
2020-10-22 11:59:56 +0800

    2nd edition

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -13,7 +13,9 @@ class RestaurantTableViewController: UITableViewController {
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     
     var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -39,11 +41,11 @@ class RestaurantTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellIdentifier = "datacell"
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell    //“as!”型態轉換
         
         //Configure the cell...
-        cell.textLabel?.text = restaurantNames[indexPath.row]   //optionial chaining
-        cell.imageView?.image = UIImage(named: restaurantImages[indexPath.row]) //image based on row number
+        cell.nameLabel.text = restaurantNames[indexPath.row]   //optionial chaining
+        cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row]) //image based on row number
         
         return cell
     }
